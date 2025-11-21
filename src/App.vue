1329343b9135d61b1879d25b66effcb1207cd082
WwwwwHM <231060382@hdu.edu.cn>
2025-11-21 15:54:55 +0800

    云图库前端初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <BasicLayout />
+    </div>
+  </a-config-provider>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from './layouts/BasicLayout.vue'
+import { healthUsingGet } from './api/mainController'
+import { useLoginUserStore } from './stores/useLoginUserStore'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import { ref } from 'vue'
+dayjs.locale('zh-cn');
+
+// 用 ref 声明响应式数据（替代原来的 data()）
+const locale = ref(zhCN);
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
+
+<style scoped></style>
